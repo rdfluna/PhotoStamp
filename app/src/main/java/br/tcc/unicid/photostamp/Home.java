21ab1b083a9f5d99021437480906eb8db7b9a73a
Rbn91 <Robson_familia21@hotmail.com>
2018-05-27 19:44:20 -0300

    Imagens e splash screen prontas

--- app/src/main/java/br/tcc/unicid/photostamp/Home.java
+++ app/src/main/java/br/tcc/unicid/photostamp/Home.java
@@ -2,6 +2,7 @@ package br.tcc.unicid.photostamp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -30,10 +31,13 @@ public class Home extends AppCompatActivity
     @Inject
     PhotoBll photoBll;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
